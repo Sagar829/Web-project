e3f6427942bd132f8f76e49aad17afad28fd27e4
Sagar829 <sagarniroula950@gmail.com>
2024-02-11 11:50:42 +0545

    first commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+  alert("I am external js");
+  console.log("This is console item");
+  var form = document.getElementById("form-data");
+  console.log(form);
+  var email = document.getElementById("email");
+  console.log(email);
